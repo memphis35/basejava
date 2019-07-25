189fda43f3c486a13210b1359fac000e3af6382f
Aleksandr Smirnov <n.nirnaeth@gmail.com>
2019-07-25 19:40:33 +0300

    HW lesson 2. Работа над ошибками. Updated.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -21,45 +21,45 @@ public class ArrayStorage {
 
     /**
      * Save resume object in array storage
-     * @param r current resume
+     * @param resume current resume
      */
-    public void save(Resume r) {
+    public void save(Resume resume) {
         if (size >= 0 && size < maxSize) {
-            if (!exist(r.getUuid())) {
-                storage[size] = r;
+            if (!exist(resume.getUuid())) {
+                storage[size] = resume;
                 System.out.println("Resume succesfully saved.");
                 size++;
             }
             if (size >= maxSize) {
                 System.out.println("Storage overloaded.");
-            }
-        }
-    }
-
-        /**
-         * Replace resume in storage by UUID number
-         * @param r incoming resume
-         */
-        public void update (Resume r) {
-            for (int i = 0; i < size; i++) {
-                if (exist(r.getUuid())) {
-                    storage[i] = r;
-                    System.out.println("Resume #" + r.getUuid() + " successfully updated.");
-                    break;
                 }
             }
-            if (!exist(r.getUuid())) {
+        }
+
+    /**
+    * Replace resume in storage by UUID number
+    * @param resume incoming resume
+    */
+    public void update (Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (exist(r.getUuid())) {
+                storage[i] = resume;
+                System.out.println("Resume #" + resume.getUuid() + " successfully updated.");
+                break;
+                }
+            }
+            if (!exist(resume.getUuid())) {
                 System.out.println("Resume #" + r.getUuid() + " not found.");
             }
         }
 
-        /**
-         * Get UUID number from Resume object, if it exist and possible
-         * @param uuid incoming UUID number
-         * @return UUID number of existing Resume
-         */
-        public Resume get (String uuid) {
-            Resume answer = new Resume();
+    /**
+    * Get UUID number from Resume object, if it exist and possible
+    * @param uuid incoming UUID number
+    * @return UUID number of existing Resume
+    */
+    public Resume get (String uuid) {
+        Resume answer = new Resume();
             for (int i = 0; i < size; i++) {
                 if (exist(uuid)) {
                     answer = storage[i];
@@ -71,39 +71,39 @@ public class ArrayStorage {
             return answer;
         }
 
-        /**
-         * Delete existing Resume from array storage by UUID number
-         * @param uuid incoming UUID number
-         */
-        public void delete (String uuid) {
-            if (!exist(uuid)) {
-                System.out.println("Resume #" + uuid + " not found.");
-            }
-            else {
-                for (int i = 0; i < size; i++) {
-                    if (exist(uuid)) {
-                        System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
-                        size--;
-                        System.out.println("Resume #" + uuid + " succesfully deleted.");
+    /**
+    * Delete existing Resume from array storage by UUID number
+    * @param uuid incoming UUID number
+    */
+    public void delete (String uuid) {
+        if (!exist(uuid)) {
+            System.out.println("Resume #" + uuid + " not found.");
+        }
+        else {
+            for (int i = 0; i < size; i++) {
+                if (exist(uuid)) {
+                    System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+                    size--;
+                    System.out.println("Resume #" + uuid + " succesfully deleted.");
                     }
                 }
             }
         }
 
-        /**
-         * Get all Resume's UUIN numbers, printed to stack
-         * @return array, contains only Resumes in storage (without null)
-         */
-        public Resume[] getAll () {
-            return Arrays.copyOf(storage, size);
+    /**
+    * Get all Resume's UUIN numbers, printed to stack
+    * @return array, contains only Resumes in storage (without null)
+    */
+    public Resume[] getAll () {
+        return Arrays.copyOf(storage, size);
         }
 
-        /**
-         * Call a current quantity of Resume of array storage
-         * @return size of array (quantity of objects)
-         */
-        public int size () {
-            return size;
+    /**
+    * Call a current quantity of Resume of array storage
+    * @return size of array (quantity of objects)
+    */
+    public int size () {
+        return size;
         }
 
     /**
@@ -111,13 +111,13 @@ public class ArrayStorage {
      * @param uuid UUID number
      * @return true if resume exist
      */
-        public boolean exist (String uuid){
-            boolean isExist = false;
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    isExist = true;
+    public boolean exist (String uuid){
+    boolean isExist = false;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                isExist = true;
                 }
             }
-            return isExist;
+        return isExist;
         }
     }
