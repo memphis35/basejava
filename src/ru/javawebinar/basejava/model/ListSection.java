5fd9ded14d4cb8a1c0c6912a0f22049565cc3cef
Memphis <43417895+memphis35@users.noreply.github.com>
2020-04-13 13:45:58 +0300

    Lesson #7. ListInfo equals() and hashcode() added.

--- src/ru/javawebinar/basejava/model/ListSection.java
+++ src/ru/javawebinar/basejava/model/ListSection.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.basejava.model;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ListSection extends Section<ArrayList<String>> {
 
@@ -23,5 +24,26 @@ public class ListSection extends Section<ArrayList<String>> {
         }
         result.deleteCharAt(result.length()-1);
         return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof ListSection)) return false;
+        ListSection list = (ListSection) obj;
+        if (data.size() != list.data.size()) return false;
+        boolean isEqual = true;
+        for (int i = 0; i < data.size(); i++) {
+            if (!data.get(i).equals(list.data.get(i))) {
+                isEqual = false;
+                break;
+            }
+        }
+        return isEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(data);
     }
 }
