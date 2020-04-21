a86a78198e3c23651c2a19f9c296696d820fcda1
Memphis <43417895+memphis35@users.noreply.github.com>
2020-04-21 09:10:34 +0300

    Lesson #7. Logging added.

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -6,30 +6,37 @@ import ru.javawebinar.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<T> implements Storage {
 
     private Comparator<Resume> comparator = Comparator.comparing(Resume::getFullName).thenComparing(Resume::getUuid);
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     public void save(Resume resume) {
         T searchKey = getSearchKey(resume.getUuid());
         if (!isExistKey(searchKey)) {
             saveToStorage(searchKey, resume);
+            LOG.info("Save " + resume);
         } else {
+            LOG.warning(resume + " already exist.");
             throw new ExistException(resume.getUuid());
         }
     }
 
     public void update(Resume resume) {
         updateToStorage(checkKey(resume.getUuid()), resume);
+        LOG.info("Update " + resume);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return getResume(checkKey(uuid));
     }
 
     public void delete(String uuid) {
         deleteFromStorage(checkKey(uuid));
+        LOG.info("Delete " + uuid);
     }
 
     public List<Resume> getAllSorted() {
@@ -43,6 +50,7 @@ public abstract class AbstractStorage<T> implements Storage {
         if (isExistKey(searchKey)) {
             return searchKey;
         } else {
+            LOG.warning(uuid + "not exist.");
             throw new NotExistException(uuid);
         }
     }
