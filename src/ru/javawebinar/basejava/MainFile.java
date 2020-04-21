5f71c4e17d21bc5865ebc9ea360c77a945eda25f
Memphis <43417895+memphis35@users.noreply.github.com>
2020-04-21 10:31:30 +0300

    Lesson #8. Recursive method to show all files added.

---
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainFile {
+
+    public static void main(String[] args) throws IOException {
+
+        File dir = new File("D:\\basejava\\src");
+        System.out.println(dir.isDirectory());
+        File[] list = dir.listFiles();
+        System.out.println(dir.getCanonicalPath());
+        if (list != null) {
+            for (File name : list) {
+                showAllFiles(name);
+            }
+        }
+    }
+
+    public static void showAllFiles(File file) throws IOException {
+        if (file.isDirectory()) {
+            for (File f : Objects.requireNonNull(file.listFiles())) {
+                showAllFiles(f);
+            }
+        }
+        if (!file.isDirectory()) System.out.println(file.getCanonicalPath());
+    }
+}
