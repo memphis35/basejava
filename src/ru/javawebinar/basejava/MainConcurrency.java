8a760a866ba1c2fa571366f6803b89913f9adfdf
Memphis <43417895+memphis35@users.noreply.github.com>
2020-05-19 07:18:54 +0300

    Lesson #11. Deadlock example has been corrected.

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -2,27 +2,36 @@ package ru.javawebinar.basejava;
 
 public class MainConcurrency {
 
-    Object lock = new Object();
+    Object lock1 = new Object();
+    Object lock2 = new Object();
 
     public static void main(String[] args) {
         MainConcurrency deadlock = new MainConcurrency();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 2; i++) {
             new Thread(deadlock::method1).start();
             new Thread(deadlock::method2).start();
         }
     }
 
     public void method1() {
-        synchronized (lock) {
-            synchronized (this) {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+            }
+            synchronized (lock2) {
                 System.out.println(Thread.currentThread().getName() + " executing method1");
             }
         }
     }
 
     public void method2() {
-        synchronized (this) {
-            synchronized (lock) {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+            }
+            synchronized (lock1) {
                 System.out.println(Thread.currentThread().getName() + " executing method2");
             }
         }
