7fecedd2f70c85ecf319b8351cf61dd2c1533b29
Memphis <43417895+memphis35@users.noreply.github.com>
2020-05-19 21:21:49 +0300

    Lesson #12. Stream methods added.

---
+++ src/ru/javawebinar/basejava/MainStream.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.basejava;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class MainStream {
+
+    static int minValue(int[] array) {
+        return Arrays.stream(array)
+                .distinct()
+                .sorted()
+                .reduce(0, (x1, x2) -> x1 * 10 + x2);
+    }
+
+    static List<Integer> oddOrEven(List<Integer> integers) {
+        final int sum = integers.stream().reduce(0, Integer::sum);
+        return integers.stream()
+                .filter(i -> {
+                    if (sum % 2 == 0) return i % 2 != 0;
+                    else return i % 2 == 0;
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minValue(new int[]{5, 7, 9, 7, 5, 3, 5, 9, 3, 2, 1})); //123579
+        List<Integer> list = new ArrayList<>();
+        Collections.addAll(list, 1, 2, 3, 4, 5, 6, 7, 8, 9); // sum = 45, print 2 4 6 8
+        oddOrEven(list).forEach(i -> System.out.print(i + " "));
+    }
+}
